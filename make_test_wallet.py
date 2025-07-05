f670a5c56862f7be256a24f4c49d0219166c4cd9
Hanashiko <hlichisper@gmail.com>
2025-07-06 01:15:56 +0300

    TMP(wallet): removed test file script

--- make_test_wallet.py
+++
@@ -1,6 +0,0 @@
-import wallet
-
-private, public = wallet.generate_keypair()
-wallet.save_keys_to_files(private, public, name="mywallet")
-address = wallet.get_address_from_public_key(public)
-print(f"your address: {address}")
