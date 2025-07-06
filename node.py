b1b953cfaaadb8d1d4ed345e38546bef5801eecb
Hanashiko <hlichisper@gmail.com>
2025-07-06 16:49:36 +0300

    feat(node): auto display peers when start node

--- node.py
+++ node.py
@@ -293,4 +293,6 @@ if __name__ == "__main__":
     announce_myself()
     sync_with_network()
         
+    print(f"Connected peers: {list(peers)}")
+        
     app.run(host='0.0.0.0', port=PORT)
